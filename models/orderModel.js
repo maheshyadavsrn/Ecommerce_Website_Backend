aa9788b6d2ffaf6d348cde879170178a3c2cf34b
maheshyadavsrn <maheshyadavsrn2022@gmail.com>
2024-06-27 14:48:57 +0530

    Your commit message

---
+++ models/orderModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
